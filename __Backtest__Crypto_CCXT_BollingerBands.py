fa70dd87e1b8fd0824a7730acf5b456230bdbfd5
aLca <>
2024-10-01 23:56:19 +0200

    Final touches

--- __Backtest__Crypto_CCXT_BollingerBands.py
+++ __Backtest__Crypto_CCXT_BollingerBands.py
@@ -1,3 +1,3 @@
-from fastquant import get_crypto_data, backtest, get_database_stock_data
+from fastquant import backtest, get_database_stock_data
 crypto = get_database_stock_data("EUR_USD", "2018-12-01", "2019-12-31", "15m")
 backtest('bbands', crypto, period=20, devfactor=2.0)

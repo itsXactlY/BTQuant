d7b40a2d31d97d27f036b95081af92461b0ab473
aLca <>
2024-10-02 00:03:30 +0200

    Unleash v2

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ source .btq/bin/activate
 pip install --upgrade pip setuptools wheel
 
 # Clone the repository
-git clone https://github.com/itsXactlY/BTQuant/tree/dev BTQuant
+git clone https://github.com/itsXactlY/BTQuant/ BTQuant
 cd BTQuant
 
 # Install backtrader dependencies

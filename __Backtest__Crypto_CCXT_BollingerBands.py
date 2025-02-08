0d7a0abf847313c3e4ab81865192bd88e3d99ac0
aLca <>
2025-02-08 15:31:50 +0100

    Tiny fixes, cleanup

--- __Backtest__Crypto_CCXT_BollingerBands.py
+++ __Backtest__Crypto_CCXT_BollingerBands.py
@@ -1,3 +1,10 @@
 from fastquant import backtest, get_crypto_data
-crypto = get_crypto_data("ETH/USDT", "2018-12-01", "2019-01-31", "15m")
-backtest('bbands', crypto, period=20, devfactor=2.0, init_cash=1000, backtest=True, plot=True, verbose=0)
+crypto = get_crypto_data("ETH/USDT", "2018-12-01", "2019-01-31", "15m", "kucoin")
+backtest('bbands', 
+         crypto, 
+         period=20, 
+         devfactor=2.0, 
+         init_cash=1000, 
+         backtest=True, 
+         plot=True, 
+         verbose=3)

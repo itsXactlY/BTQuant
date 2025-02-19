35066997fed05839d0c11ddc89ff313e483c490a
aLca <>
2025-02-19 20:50:47 +0100

    First machine learning strategy dropped of the candy van. It was actually planned as a “keep it simple stupid” proof of concept. © by aLca (itsXactlY) // BTQuant

--- __Backtest__CCXT_AllStrategys.py
+++ __Backtest__CCXT_AllStrategys.py
@@ -12,3 +12,4 @@ backtest('qqe', crypto, init_cash=1000, backtest=True, plot=True, verbose=0)
 backtest('OrChainKioseff', crypto, init_cash=1000, backtest=True, plot=True, verbose=1)
 backtest('msa', crypto, init_cash=1000, backtest=True, plot=True, verbose=0)
 backtest('STScalp', crypto, init_cash=1000, backtest=True, plot=True, verbose=0)
+backtest('NNRQK', crypto, init_cash=1000, backtest=True, plot=True, verbose=0) # pip install scikit-learn

3a58fd43df3b748ee5917d020b27fd5d94905a0a
aLca <>
2025-02-21 15:03:16 +0100

    More typo fixes...

--- dependencies/fastquant/strategys/__init__.py
+++ dependencies/fastquant/strategys/__init__.py
@@ -1,18 +1,18 @@
-# Modules available for fastquant.strategies.*
+# Modules available for fastquant.strategys.*
 
-from fastquant.strategies.base import BaseStrategy
-from fastquant.strategies.bollinger_band import BBandsStrategy
-from fastquant.strategies.buy_and_hold import BuyAndHoldStrategy
-from fastquant.strategies.ma_crossover import SMACStrategy, EMACStrategy
-from fastquant.strategies.macd import MACDStrategy
-from fastquant.strategies.rsi import RSIStrategy
-from fastquant.strategies.sentiment import SentimentStrategy
-from fastquant.strategies.custom import CustomStrategy, TernaryStrategy
-from fastquant.strategies.QQE_Hullband_VolumeOsc import QQE_Example
-from fastquant.strategies.Order_Chain_Kioseff_Trading import Order_Chain_Kioseff_Trading
-from fastquant.strategies.SMA_Cross_MESAdaptive_Prime import SMA_Cross_MESAdaptivePrime
-from fastquant.strategies.SuperTrend_Scalp import SuperSTrend_Scalper
-from fastquant.strategies.pancakeswap_dca_marketmaker import Pancakeswap_dca_mm
-from fastquant.strategies.pancakeswap_orders import PancakeSwapV2DirectOrderBase
-from fastquant.strategies.Aligator_supertrend import AliG_STrend
-from fastquant.strategies.NearestNeighbors_RationalQuadraticKernel import NearestNeighbors_RationalQuadraticKernel_DCA_Strategy
+from fastquant.strategys.base import BaseStrategy
+from fastquant.strategys.bollinger_band import BBandsStrategy
+from fastquant.strategys.buy_and_hold import BuyAndHoldStrategy
+from fastquant.strategys.ma_crossover import SMACStrategy, EMACStrategy
+from fastquant.strategys.macd import MACDStrategy
+from fastquant.strategys.rsi import RSIStrategy
+from fastquant.strategys.sentiment import SentimentStrategy
+from fastquant.strategys.custom import CustomStrategy, TernaryStrategy
+from fastquant.strategys.QQE_Hullband_VolumeOsc import QQE_Example
+from fastquant.strategys.Order_Chain_Kioseff_Trading import Order_Chain_Kioseff_Trading
+from fastquant.strategys.SMA_Cross_MESAdaptive_Prime import SMA_Cross_MESAdaptivePrime
+from fastquant.strategys.SuperTrend_Scalp import SuperSTrend_Scalper
+from fastquant.strategys.pancakeswap_dca_marketmaker import Pancakeswap_dca_mm
+from fastquant.strategys.pancakeswap_orders import PancakeSwapV2DirectOrderBase
+from fastquant.strategys.Aligator_supertrend import AliG_STrend
+from fastquant.strategys.NearestNeighbors_RationalQuadraticKernel import NearestNeighbors_RationalQuadraticKernel_DCA_Strategy

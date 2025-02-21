b4642f4ece9a1ec9d175a02aa0996a08b1e61c2b
aLca <>
2025-02-21 14:54:16 +0100

    Bugfixes, typos, and more...

--- dependencies/fastquant/setup.py
+++ dependencies/fastquant/setup.py
@@ -5,16 +5,20 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="fastquant",
-    version="0.1.8.1",
+    version="0.1.8.2",
     author="Lorenzo Ampil",
     author_email="lorenzo.ampil@gmail.com",
     description="Bringing data driven investments to the mainstream",
     long_description_content_type="text/markdown",
     url="https://github.com/enzoampil/fastquant",
     packages=setuptools.find_packages(exclude=["docs", "tests"]),
-    package_data={"fastquant": ["data/*"]},
+    package_data={
+        "fastquant": ["data/*"],
+        "fastquant.strategys": ["*"],
+        "fastquant.strategys.*": ["*"]
+    },
     include_package_data=True,
-    # scripts=["get_disclosures", "update_cache"],  # Adjust path if needed
+
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",

901d7083894b95c9570f9282ac3842da520209cc
aLca <>
2025-02-25 00:02:24 +0100

    Mexc Exchange support

---
+++ __LIVE__Mexc.py
@@ -0,0 +1,22 @@
+from fastquant import livetrade_crypto_mexc as livetrade
+
+# Mexc.com Exchange
+_coin = 'PI'
+_collateral = 'USDT'
+_exchange = 'mimic' # Exchange Name
+_account = 'mexc_testing' # JackRabbitRelay Account name
+_asset = f'{_coin}/{_collateral}'
+_amount = '40'
+_amount = float(_amount)
+
+livetrade(
+    coin=_coin,
+    collateral=_collateral,
+    strategy="STScalp",
+    exchange=_exchange,
+    account=_account,
+    asset=_asset,
+    amount=_amount,
+    enable_alerts=True,
+    alert_channel=-100123456789 # Telegram channel/group
+)

f2e38dbca3a7ed2a5f5c15d39c6b4a7bf174be20
aLca <>
2025-03-07 03:27:33 +0100

    Bitget livetrading example

---
+++ __LIVE__Bitget.py
@@ -0,0 +1,23 @@
+from fastquant import livetrade_crypto_bitget as livetrade
+from fastquant.strategys.SuperTrend_Scalp import SuperSTrend_Scalper
+
+
+_coin = 'PI'
+_collateral = 'USDT'
+_exchange = 'mimic' # JackRabbitRelay Exchange Name
+_account = 'bitget' # JackRabbitRelay Account name
+_asset = f'{_coin}/{_collateral}'
+_amount = '40'
+_amount = float(_amount)
+
+livetrade(
+    coin=_coin,
+    collateral=_collateral,
+    strategy=SuperSTrend_Scalper,
+    exchange=_exchange,
+    account=_account,
+    asset=_asset,
+    amount=_amount,
+    enable_alerts=False,
+    alert_channel=-100123456789
+)

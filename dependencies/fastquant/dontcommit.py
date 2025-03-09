0443b5b9bdfdfbc74084b04a231803d51007e521
aLca <>
2025-03-09 18:38:38 +0100

    Fresh Setup oopsies - im on it

--- dependencies/fastquant/dontcommit.py
+++ dependencies/fastquant/dontcommit.py
@@ -1,142 +1,145 @@
 import backtrader as bt
 import joblib
 
+# Jackrabbit Relay
 identify = ""
 jrr_webhook_url = "http://127.0.0.1:80"
+jrr_order_history = "/home/JackrabbitRelay2/Data/Mimic/"
+jrr_order_history_sub1 = ""
 
+# Web3
 bsc_privaccount1 = ""
 bsc_privaccountaddress = ""
 
+#Discord
 discord_webhook_url = ''
+discord_BTQ_url = ''
 
-telegram_api_id = 123456789
-telegram_api_hash = "abc123456"
+# Telegram (Get credentials via https://my.telegram.org/auth )
+telegram_api_id = 123
+telegram_api_hash = ""
 telegram_session_file = ".base.session"
-telegram_channel_id = -100 # -100 is important infront of for channel/groups
-
-import pandas as pd
-
-# MsSQL is now optional - if you want to use it, you need to uncomment the following lines
-'''
-import importlib
-import sys
-import os
-module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data/mssql/MsSQL/fast_mssql.cpython-313-x86_64-linux-gnu.so'))
-spec = importlib.util.spec_from_file_location("fast_mssql", module_path)
-fast_mssql = importlib.util.module_from_spec(spec)
-sys.modules["fast_mssql"] = fast_mssql
-spec.loader.exec_module(fast_mssql)
+telegram_channel_debug = -100
+telegram_channel_machinelearning = -100
 
 
+'''HACK :: MsSQL is now optional - if you want to use it, you need to uncomment the following lines'''
+# import importlib
+# import sys
+# import os
+# import pandas as pd
+# module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data/mssql/MsSQL/fast_mssql.cpython-313-x86_64-linux-gnu.so'))
+# spec = importlib.util.spec_from_file_location("fast_mssql", module_path)
+# fast_mssql = importlib.util.module_from_spec(spec)
+# sys.modules["fast_mssql"] = fast_mssql
+# spec.loader.exec_module(fast_mssql)
 
-# SQL Server connection details
-server = ''
-database = ''
-username = ''
-password = ''
-driver = '{ODBC Driver 18 for SQL Server}'
+# # SQL Server connection details
+# server = 'localhost'
+# database = 'BacktraderData'
+# username = ''
+# password = ''
+# driver = '{ODBC Driver 18 for SQL Server}'  # Adjust the driver version if necessary
 
-connection_string = (f'DRIVER={driver};'
-                     f'SERVER={server};'
-                     f'DATABASE={database};'
-                     f'UID={username};'
-                     f'PWD={password};'
-                     f'TrustServerCertificate=yes;')
+# connection_string = (f'DRIVER={driver};'
+#                      f'SERVER={server};'
+#                      f'DATABASE={database};'
+#                      f'UID={username};'
+#                      f'PWD={password};'
+#                      f'TrustServerCertificate=yes;')
 
-class MSSQLData(bt.feeds.PandasData):
-  @classmethod
-  def get_data_from_db(cls, connection_string, coin, timeframe, start_date, end_date):
-      start_timestamp = int(start_date.timestamp() * 1000)
-      end_timestamp = int(end_date.timestamp() * 1000)
+# class MSSQLData(bt.feeds.PandasData):
+#   @classmethod
+#   def get_data_from_db(cls, connection_string, coin, timeframe, start_date, end_date):
+#       start_timestamp = int(start_date.timestamp() * 1000)
+#       end_timestamp = int(end_date.timestamp() * 1000)
 
-      query = f"""
-      SELECT 
-          TimestampStart, 
-          [Open], 
-          [High], 
-          [Low], 
-          [Close], 
-          Volume
-      FROM {coin}
-      WHERE Timeframe = '{timeframe}'
-      AND TimestampStart BETWEEN {start_timestamp} AND {end_timestamp}
-      ORDER BY TimestampStart
-      OPTION(USE HINT('ENABLE_PARALLEL_PLAN_PREFERENCE'))
-      """
+#       query = f"""
+#       SELECT 
+#           TimestampStart, 
+#           [Open], 
+#           [High], 
+#           [Low], 
+#           [Close], 
+#           Volume
+#       FROM {coin}
+#       WHERE Timeframe = '{timeframe}'
+#       AND TimestampStart BETWEEN {start_timestamp} AND {end_timestamp}
+#       ORDER BY TimestampStart
+#       OPTION(USE HINT('ENABLE_PARALLEL_PLAN_PREFERENCE'))
+#       """
       
-      data = fast_mssql.fetch_data_from_db(connection_string, query)
+#       data = fast_mssql.fetch_data_from_db(connection_string, query)
       
-      df = pd.DataFrame(data, columns=['TimestampStart', 'Open', 'High', 'Low', 'Close', 'Volume'])
-      df['TimestampStart'] = pd.to_datetime(df['TimestampStart'].astype(int), unit='ms')
-      numeric_columns = ['Open', 'High', 'Low', 'Close', 'Volume']
-      df[numeric_columns] = df[numeric_columns].astype(float)
-      df.set_index('TimestampStart', inplace=True)
-      return df
+#       df = pd.DataFrame(data, columns=['TimestampStart', 'Open', 'High', 'Low', 'Close', 'Volume'])
+#       df['TimestampStart'] = pd.to_datetime(df['TimestampStart'].astype(int), unit='ms')
+#       numeric_columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+#       df[numeric_columns] = df[numeric_columns].astype(float)
+#       df.set_index('TimestampStart', inplace=True)
+#       return df
 
-  @classmethod
-  def get_all_pairs(cls, connection_string):
-      query = "SELECT DISTINCT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'"
-      data = fast_mssql.fetch_data_from_db(connection_string, query)
-      return [row[0] for row in data]
+#   @classmethod
+#   def get_all_pairs(cls, connection_string):
+#       query = "SELECT DISTINCT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'"
+#       data = fast_mssql.fetch_data_from_db(connection_string, query)
+#       return [row[0] for row in data]
 
 
-import pyodbc
-import pandas as pd
-from datetime import datetime
+# import pyodbc
+# import pandas as pd
+# from datetime import datetime
 
-class MSSQLData_Stocks(bt.feeds.PandasData):
-    @classmethod
-    def get_data_from_db(cls, connection_string, coin, timeframe, start_date, end_date):
-        # Convert datetime objects to string format
-        start_str = start_date.strftime("%Y-%m-%d %H:%M:%S")
-        end_str = end_date.strftime("%Y-%m-%d %H:%M:%S")
+# class MSSQLData_Stocks(bt.feeds.PandasData):
+#     @classmethod
+#     def get_data_from_db(cls, connection_string, coin, timeframe, start_date, end_date):
+#         # Convert datetime objects to string format
+#         start_str = start_date.strftime("%Y-%m-%d %H:%M:%S")
+#         end_str = end_date.strftime("%Y-%m-%d %H:%M:%S")
 
-        query = f"""
-        SELECT 
-            TimestampStart, 
-            [Open], 
-            [High], 
-            [Low], 
-            [Close], 
-            Volume
-        FROM {coin}
-        WHERE Timeframe = '{timeframe}'
-        AND TimestampStart BETWEEN '{start_str}' AND '{end_str}'
-        ORDER BY TimestampStart
-        OPTION(USE HINT('ENABLE_PARALLEL_PLAN_PREFERENCE'))
-        """
+#         query = f"""
+#         SELECT 
+#             TimestampStart, 
+#             [Open], 
+#             [High], 
+#             [Low], 
+#             [Close], 
+#             Volume
+#         FROM {coin}
+#         WHERE Timeframe = '{timeframe}'
+#         AND TimestampStart BETWEEN '{start_str}' AND '{end_str}'
+#         ORDER BY TimestampStart
+#         OPTION(USE HINT('ENABLE_PARALLEL_PLAN_PREFERENCE'))
+#         """
         
-        data = fast_mssql.fetch_data_from_db(connection_string, query)
+#         data = fast_mssql.fetch_data_from_db(connection_string, query)
         
-        df = pd.DataFrame(data, columns=['TimestampStart', 'Open', 'High', 'Low', 'Close', 'Volume'])
-        df['TimestampStart'] = pd.to_datetime(df['TimestampStart'])
-        numeric_columns = ['Open', 'High', 'Low', 'Close', 'Volume']
-        df[numeric_columns] = df[numeric_columns].astype(float)
-        df.set_index('TimestampStart', inplace=True)
-        return df
+#         df = pd.DataFrame(data, columns=['TimestampStart', 'Open', 'High', 'Low', 'Close', 'Volume'])
+#         df['TimestampStart'] = pd.to_datetime(df['TimestampStart'])
+#         numeric_columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+#         df[numeric_columns] = df[numeric_columns].astype(float)
+#         df.set_index('TimestampStart', inplace=True)
+#         return df
 
-    @classmethod
-    def get_all_pairs(cls, connection_string):
-        query = "SELECT DISTINCT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'"
-        data = fast_mssql.fetch_data_from_db(connection_string, query)
-        return [row[0] for row in data]
+#     @classmethod
+#     def get_all_pairs(cls, connection_string):
+#         query = "SELECT DISTINCT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'"
+#         data = fast_mssql.fetch_data_from_db(connection_string, query)
+#         return [row[0] for row in data]
 
 
-    @classmethod
-    def get_all_pairs(cls, connection_string):
-        query = "SELECT DISTINCT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'"
-        try:
-            with pyodbc.connect(connection_string) as conn:
-                data = pd.read_sql(query, conn)
-            return data['TABLE_NAME'].tolist()
-        except pyodbc.Error as e:
-            print(f"Database error occurred: {str(e)}")
-            return []
-        except Exception as e:
-            print(f"An unexpected error occurred: {str(e)}")
-            return []
-
-'''
+#     @classmethod
+#     def get_all_pairs(cls, connection_string):
+#         query = "SELECT DISTINCT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'"
+#         try:
+#             with pyodbc.connect(connection_string) as conn:
+#                 data = pd.read_sql(query, conn)
+#             return data['TABLE_NAME'].tolist()
+#         except pyodbc.Error as e:
+#             print(f"Database error occurred: {str(e)}")
+#             return []
+#         except Exception as e:
+#             print(f"An unexpected error occurred: {str(e)}")
+#             return []
 # End of MsSQL
 
 def ptu():

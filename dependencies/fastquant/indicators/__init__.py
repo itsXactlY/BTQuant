964207914d63d47410eb1c3c705175e3ef771b45
aLca <>
2025-04-24 01:55:32 +0200

    Wipe

--- dependencies/fastquant/indicators/__init__.py
+++
@@ -1,8 +0,0 @@
-# Modules available for fastquant.indicators.*
-
-from fastquant.indicators.sentiment import Sentiment
-from fastquant.indicators.custom import CustomIndicator
-
-
-# Import backtrader indicators
-from fastquant.indicators.backtrader_indicators import *

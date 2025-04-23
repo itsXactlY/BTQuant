eadaefeb44a3a73ff0f6e1078cc9c1a023d309ef
aLca <>
2025-04-24 01:57:22 +0200

    Round 3 - An new beginning [Linux Only]

---
+++ dependencies/MsSQL/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, Extension
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+ext_modules = [
+    Pybind11Extension(
+        "fast_mssql",
+        ["fast_mssql.cpp"],
+        libraries=["odbc"],
+        extra_compile_args=["-O3"],
+    ),
+]
+
+setup(
+    name="fast_mssql",
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+)

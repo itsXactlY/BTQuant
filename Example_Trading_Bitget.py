2ded5850c2774090c0103fadcde0c77e14eb34bb
aLca <>
2025-04-26 23:23:44 +0200

    Time to set exchanges on fire

--- Example_Trading_Bitget.py
+++ Example_Trading_Bitget.py
@@ -1,19 +1,17 @@
-from backtrader.stores.binance_store import BinanceStore
-from backtrader.imports import dt, bt
+'''
+IMPORTANT NOTE ABOUT IMPORTED STRATEGYS IN THIS FILE - LOAD OR IMPORT ONLY THAT PARTICULAR STRATEGY U USE!
+BACKTRADER WARMING UP EVERY POSSIBLE STRATEGY WHAT IS DECLARED AS IMPORT HERE!
+CAUSING ALOT OF WARMUP TIME, MEMORY CONSUMPTION, INDICATORS, AND EVERYTHING BEYONED (TIME IS MONEY!)
+'''
 
-'''
-############## IMPORTANT NOTE ABOUT IMPORTED STRATEGYS IN THIS FILE - LOAD OR IMPORT ONLY THAT PARTICULAR STRATEGY U USE! ##############
-############## BACKTRADER WARMING UP EVERY POSSIBLE STRATEGY WHAT IS DECLARED AS IMPORT HERE! ##############
-############## CAUSING ALOT OF WARMUP TIME, MEMORY CONSUMPTION, INDICATORS, AND EVERYTHING BEYONED (TIME IS MONEY!) ##############
-'''
+from backtrader.stores.bitget_store import BitgetStore
+from backtrader.imports import dt, bt
 from backtrader.strategies.NearestNeighbors_RationalQuadraticKernel import NRK
 
-
-
 # JackRabbitRelay
-_coin = 'GAS'
+_coin = 'FLOKI'
 _collateral = 'USDT'
-_exchange = 'binance'
+_exchange = 'Bitget'
 _account = '' #_account = '<JackRabbit_SubaccountName>'
 _asset = f'{_coin}/{_collateral}'
 _amount = '11'
@@ -21,7 +19,7 @@ _amount = float(_amount)
 
 def run():
     cerebro = bt.Cerebro(quicknotify=True)
-    store = BinanceStore(
+    store = BitgetStore(
         coin_refer=_coin,
         coin_target=_collateral
         )
---
+++ Example_Trading_Mexc.py
@@ -0,0 +1,49 @@
+'''
+IMPORTANT NOTE ABOUT IMPORTED STRATEGYS IN THIS FILE - LOAD OR IMPORT ONLY THAT PARTICULAR STRATEGY U USE!
+BACKTRADER WARMING UP EVERY POSSIBLE STRATEGY WHAT IS DECLARED AS IMPORT HERE!
+CAUSING ALOT OF WARMUP TIME, MEMORY CONSUMPTION, INDICATORS, AND EVERYTHING BEYONED (TIME IS MONEY!)
+'''
+from backtrader.stores.mexc_store import MexcStore
+from backtrader.imports import dt, bt
+from backtrader.strategies.NearestNeighbors_RationalQuadraticKernel import NRK
+
+# JackRabbitRelay
+_coin = 'FLOKI'
+_collateral = 'USDT'
+_exchange = 'Mexc'
+_account = 'binance_sub1' #_account = '<JackRabbit_SubaccountName>'
+_asset = f'{_coin}/{_collateral}'
+_amount = '11'
+_amount = float(_amount)
+
+def run():
+    cerebro = bt.Cerebro(quicknotify=True)
+    store = MexcStore(
+        coin_refer=_coin,
+        coin_target=_collateral
+        )
+
+    from_date = dt.datetime.now(dt.timezone.utc) - dt.timedelta(minutes=6*15)
+    data = store.getdata(start_date=from_date)
+    data._dataname = f"{_coin}{_collateral}"
+    cerebro.addstrategy(NRK,
+                        exchange=_exchange,
+                        account=_account,
+                        asset=_asset,
+                        amount=_amount,
+                        coin=_coin,
+                        collateral=_collateral,
+                        enable_alerts=False,
+                        alert_channel=-100,
+                        backtest=False)
+    cerebro.adddata(data=data, name=data._dataname)
+    cerebro.run(live=True)
+
+if __name__ == '__main__':
+    try:
+        run()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        import traceback
+        print("Full traceback:")
+        traceback.print_exc()

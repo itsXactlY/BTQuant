fe6b59ee4ac46a30fb49a290fd8d52d2d98e838f
aLca <>
2025-05-16 10:31:27 +0200

    Enhance backtesting example with CCXT data; update strategies and parameters; improve debug output and caching settings in NRK and Vumanchu strategies.

--- Example_Backtest_CCXT.py
+++ Example_Backtest_CCXT.py
@@ -1,8 +1,17 @@
+'''
+This is a simple example of how to run a backtest using BTQuant with CCXT data, and some of the custom strategies. There is no limit of what BTQ cant do.
+More Strategies can be explored in the backtrader/strategies folder.
+This example uses the Order_Chain_Kioseff_Trading strategy, but you can also use the VuManchCipher_A and VuManchCipher_B strategies.
+You can also use the get_crypto_data function to get data from any exchange, and any pair.
+'''
 from backtrader import backtest, get_crypto_data
-from backtrader.strategies.QQE_Hullband_VolumeOsc import QQE_Example
+from backtrader.strategies.NearestNeighbors_RationalQuadraticKernel import NRK
+from backtrader.strategies.Order_Chain_Kioseff_Trading import Order_Chain_Kioseff_Trading
+from backtrader.strategies.Vumanchu_A import VuManchCipher_A
+from backtrader.strategies.Vumanchu_B import VuManchCipher_B
 
-data = get_crypto_data("ETH/USDT", "2018-12-01", "2019-01-31", "15m", "kucoin")
-backtest(QQE_Example, 
+data = get_crypto_data("XRP/USDT", "2025-01-01", "2025-05-15", "1m", "binance")
+backtest(VuManchCipher_A, 
         data,
         init_cash=1000, 
         backtest=True, 

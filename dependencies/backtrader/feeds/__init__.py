30f02a7a4571a5973084ce4afb303ee14fc02350
aLca <>
2025-05-19 18:31:03 +0200

    Refactor broker and feed imports to improve code clarity and reduce unnecessary try-except blocks

--- dependencies/backtrader/feeds/__init__.py
+++ dependencies/backtrader/feeds/__init__.py
@@ -20,42 +20,12 @@
 ###############################################################################
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-
-
-from .csvgeneric import *
-from .btcsv import *
-from .vchartcsv import *
-from .vchart import *
-from .yahoo import *
-from .quandl import *
-from .sierrachart import *
-from .mt4csv import *
 from .pandafeed import *
-from .influxfeed import *
-# from .mssql_crypto import *
-# from .mssql_stocks import *
-# from .crypto import *
-try:
-    from .ibdata import *
-except ImportError:
-    pass  # The user may not have ibpy installed
-
-try:
-    from .vcdata import *
-except ImportError:
-    pass  # The user may not have something installed
-
-try:
-    from .oanda import OandaData
-except ImportError:
-    pass  # The user may not have something installed
 
 try:
     from .ccxt import CCXT
 except ImportError:
     pass # The user may not have something installed
-
-from .vchartfile import VChartFile
 
 from .rollover import RollOver
 from .chainer import Chainer
--- dependencies/backtrader/stores/__init__.py
+++ dependencies/backtrader/stores/__init__.py
@@ -21,27 +21,3 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-# The modules below should/must define __all__ with the objects wishes
-# or prepend an "_" (underscore) to private classes/variables
-
-try:
-    from .ibstore import IBStore
-except ImportError:
-    pass  # The user may not have ibpy installed
-
-try:
-    from .vcstore import VCStore
-except ImportError:
-    pass  # The user may not have a module installed
-
-try:
-    from .oandastore import OandaStore
-except ImportError:
-    pass  # The user may not have a module installed
-
-
-from .vchartfile import VChartFile
-
-# from .binance_store import BinanceStore
-# from .bitget_store import BitgetStore
-# from .mexc_store import MexcStore

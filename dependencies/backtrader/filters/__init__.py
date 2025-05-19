707ae4db267dcdfdfb97c65d996be2754bb70d97
aLca <>
2025-05-19 18:31:03 +0200

    Refactor broker and feed imports to improve code clarity and reduce unnecessary try-except blocks

--- dependencies/backtrader/filters/__init__.py
+++ dependencies/backtrader/filters/__init__.py
@@ -26,9 +26,3 @@ from .. import Filter
 
 from .datafilter import *
 from .datafiller import *
-from .session import *
-from .calendardays import *
-from .daysteps import *
-from .bsplitter import *
-from .heikinashi import *
-from .renko import *

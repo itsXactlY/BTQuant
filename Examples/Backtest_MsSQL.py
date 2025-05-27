aa59a0491a591cda42cc368410a7b17a655e6c13
aLca <>
2025-05-27 11:40:35 +0200

    Refactor backtest asset handling to improve clarity and consistency across examples

--- Examples/Backtest_MsSQL.py
+++ Examples/Backtest_MsSQL.py
@@ -1,19 +1,17 @@
-'''
-############## IMPORTANT NOTE ABOUT IMPORTED STRATEGYS IN THIS FILE - LOAD OR IMPORT ONLY THAT PARTICULAR STRATEGY U USE! ##############
-############## BACKTRADER WARMING UP EVERY POSSIBLE STRATEGY WHAT IS DECLARED AS IMPORT HERE! ##############
-############## CAUSING ALOT OF WARMUP TIME, MEMORY CONSUMPTION, INDICATORS, AND EVERYTHING BEYONED (TIME IS MONEY!) ##############
-'''
-
 # from backtrader.strategies.NearestNeighbors_RationalQuadraticKernel import NRK
 from backtrader.strategies.ST_RSX_ASI import STrend_RSX_AccumulativeSwingIndex
 from backtrader.utils.backtest import backtest
 from backtrader.feeds.mssql_crypto import get_database_data
 
-data = get_database_data("XRP", "2024-06-01", "2025-01-08", "1m")
+_coin = "XRP"
+_collateral = 'USDT'
+_asset = f'{_coin}/{_collateral}'
+
+data = get_database_data(_coin, "2024-06-01", "2025-01-08", "1m")
 
 if __name__ == '__main__':
     try:
-        backtest(STrend_RSX_AccumulativeSwingIndex, data, init_cash=1000, backtest=True, quantstats=True, plot=True)
+        backtest(STrend_RSX_AccumulativeSwingIndex, data, init_cash=1000, backtest=True, quantstats=True, plot=True, asset_name=_asset)
     except Exception as e:
         print(f"An error occurred: {e}")
         import traceback

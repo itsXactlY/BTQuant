d102bdaa208e6060f8f5e0bb52f20ae09a128e40
Dohn Joe <>
2025-08-31 19:21:01 +0200

    Enhance trading strategy with improved signal quality filters and DCA logic; adjust parameters for better performance

--- dependencies/backtrader/dontcommit.py
+++ dependencies/backtrader/dontcommit.py
@@ -1,16 +1,21 @@
 import backtrader as bt
 import joblib
+import fast_mssql
+
+### Live trading / paper trading settings
 
 # JackRabbit Relay
 identify = "" # Fill Identify string from JRR Setup
 jrr_webhook_url = "http://127.0.0.1:80"
 jrr_order_history = "/home/JackrabbitRelay2/Data/Mimic/"
 
-# Web3
+# Web3 / BSC TODO :: Move to secrets file, add full documentation
 bsc_privaccount1 = ""
 bsc_privaccountaddress = ""
 
-#Discord
+### Alerts
+
+# Discord
 discord_webhook_url = '' #'https://discord.com/api/webhooks/...'
 
 # Telegram
@@ -19,23 +24,25 @@ telegram_api_hash = ""
 telegram_session_file = ".base.session"
 telegram_channel = -100
 
-# Precompiled C++ MSSQL Adapter
-import importlib
-import sys
-import os
-import pandas as pd
-import fast_mssql
-
+### Backtesting settings
 # SQL Server connection details
 server = 'localhost'
-database = 'BinanceData'
+candle_database = 'BinanceData'
+optuna_database = 'OptunaBT'
 username = 'SA'
 password = 'YourStrong!Passw0rd'
 driver = '{ODBC Driver 18 for SQL Server}'  # Adjust the driver version if necessary
 
 connection_string = (f'DRIVER={driver};'
                      f'SERVER={server};'
-                     f'DATABASE={database};'
+                     f'DATABASE={candle_database};'
+                     f'UID={username};'
+                     f'PWD={password};'
+                     f'TrustServerCertificate=yes;')
+
+optuna_connection_string = (f'DRIVER={driver};'
+                     f'SERVER={server};'
+                     f'DATABASE={optuna_database};'
                      f'UID={username};'
                      f'PWD={password};'
                      f'TrustServerCertificate=yes;')

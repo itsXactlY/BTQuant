e33e27370e9b103b346af4f2b227a21351c4c88e
Alca <alca@pubbtquant.com>
2026-01-29 15:30:33 +0100

    cleanup

---
+++ dependencies/BTQ_Render_Engine/build_my_test/_deps/implot-subbuild/CMakeFiles/implot-populate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/implot-populate"
+  "CMakeFiles/implot-populate-complete"
+  "implot-populate-prefix/src/implot-populate-stamp/implot-populate-build"
+  "implot-populate-prefix/src/implot-populate-stamp/implot-populate-configure"
+  "implot-populate-prefix/src/implot-populate-stamp/implot-populate-download"
+  "implot-populate-prefix/src/implot-populate-stamp/implot-populate-install"
+  "implot-populate-prefix/src/implot-populate-stamp/implot-populate-mkdir"
+  "implot-populate-prefix/src/implot-populate-stamp/implot-populate-patch"
+  "implot-populate-prefix/src/implot-populate-stamp/implot-populate-test"
+  "implot-populate-prefix/src/implot-populate-stamp/implot-populate-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/implot-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

22a3f50834c10d0b0f49f28b29583e13686c97f3
ralphy/agent-9-1769808907781-lmge6m <ralphy.agent.9@example.com>
2026-01-31 18:05:45 +0100

    cleanup

--- .ralphy-sandboxes/agent-2-eao498/dependencies/ccapi/format_cpp.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-find . -type f -not -path "*/build/*" \( -name "*.h" -or -name "*.cpp" -or -name "*.hpp" \) -exec clang-format -i -style=file {} \+
--- .ralphy-sandboxes/agent-2-eao498/dependencies/ccapi/format_javascript.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-find . -type f -path "*/binding/*" -not -path "*/build/*" -name "*.js" -exec clang-format -i -style=file {} \+
